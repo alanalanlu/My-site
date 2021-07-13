6b5a5ba2bef57d3c3ec5e86bbf40cd9abaf6b382
alanalanlu <46388471+alanalanlu@users.noreply.github.com>
2021-07-13 14:04:36 -0700

    fix UI

--- bideo.js
+++ bideo.js
@@ -1,10 +1,3 @@
-/**
- * Full Background Video
- *
- * More info on Audio/Video Media Events/Attributes/Methods
- * - https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
- * - http://www.w3schools.com/tags/ref_av_dom.asp
- */
 
 (function (global) {
 
@@ -17,14 +10,7 @@
     this.videoEl = null;
 
     // Approximate Loading Rate
-    //
-    // The value will be a number like 0.8
-    // which means to load 4 seconds of the video
-    // it takes 5 seconds. If the number is super low
-    // like 0.2 (regular 3g connections) then you can
-    // decide whether to play the video or not.
-    // This behaviour will be controller with
-    // the `acceptableLoadingRate` option.
+
     this.approxLoadingRate = null;
 
     // Methods to which `this` will be bound
@@ -51,8 +37,6 @@
       // Meta data event
       self.videoEl.addEventListener('loadedmetadata', self._resize, false);
 
-      // Fired when enough has been buffered to begin the video
-      // self.videoEl.readyState === 4 (HAVE_ENOUGH_DATA)
       self.videoEl.addEventListener('canplay', function () {
         // Play the video when enough has been buffered
         if (!self.opt.isMobile) {
@@ -131,16 +115,11 @@
       // Will be more than 1 if W > H and less if H > W
       var videoRatio = (w / h).toFixed(2);
 
-      // Get the container DOM element and its styles
-      //
-      // Also calculate the min dimensions required (this will be
-      // the container dimentions)
       var container = this.opt.container
         , containerStyles = global.getComputedStyle(container)
         , minW = parseInt( containerStyles.getPropertyValue('width') )
         , minH = parseInt( containerStyles.getPropertyValue('height') );
 
-      // If !border-box then add paddings to width and height
       if (containerStyles.getPropertyValue('box-sizing') !== 'border-box') {
         var paddingTop = containerStyles.getPropertyValue('padding-top')
           , paddingBottom = containerStyles.getPropertyValue('padding-bottom')
@@ -156,24 +135,10 @@
         minH += paddingTop + paddingBottom;
       }
 
-      // What's the min:intrinsic dimensions
-      //
-      // The idea is to get which of the container dimension
-      // has a higher value when compared with the equivalents
-      // of the video. Imagine a 1200x700 container and
-      // 1000x500 video. Then in order to find the right balance
-      // and do minimum scaling, we have to find the dimension
-      // with higher ratio.
-      //
-      // Ex: 1200/1000 = 1.2 and 700/500 = 1.4 - So it is best to
-      // scale 500 to 700 and then calculate what should be the
-      // right width. If we scale 1000 to 1200 then the height
-      // will become 600 proportionately.
       var widthRatio = minW / w;
       var heightRatio = minH / h;
 
-      // Whichever ratio is more, the scaling
-      // has to be done over that dimension
+
       if (widthRatio > heightRatio) {
         var new_width = minW;
         var new_height = Math.ceil( new_width / videoRatio );
